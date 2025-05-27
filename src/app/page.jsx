7e47a0e07bf638515180ab3d1c62f69a1b144b31
elvxk <pradiptaramadan9c@gmail.com>
2025-05-28 02:02:03 +0700

    feat(api): new api url

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,22 @@
+"use client";
 import Footer from "@/components/Footer";
 import ToolCard from "@/components/ToolCard";
 import getData from "@/lib/getData";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
-const Home = async () => {
-  const data = await getData();
+const Home = () => {
+  const url = "https://api.pradipta.dev";
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(`${url}/projects`, { method: "GET" })
+      .then((res) => res.json())
+      .then(({ data }) => {
+        setData(data.filter((project) => project.is_tool));
+      });
+  }, []);
+
+  // const data = await getData();
   return (
     <div className="container mx-auto px-6">
       <div className="flex gap-6 flex-col items-center justify-center min-h-[70vh]">
@@ -14,14 +26,16 @@ const Home = async () => {
             alt="ELVXK TOOL"
             width={100}
             height={100}
+            priority
           />
           <h1 className="text-2xl font-bold">ELVXK TOOLS</h1>
           <h1 className="">Tools made by elvxk</h1>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full">
-          {data.map((project) => {
-            return <ToolCard project={project} key={project.id} />;
-          })}
+          {data &&
+            data.map((project) => {
+              return <ToolCard project={project} key={project.id} />;
+            })}
         </div>
       </div>
     </div>
